fa9f584f5ad200b8862aa801a59382f882eda0ed
panferov-2010 <panferov-2010@mail.ru>
2021-10-23 23:05:13 +0600

    Add show all books by each author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -56,6 +56,11 @@ class AuthorsController < ApplicationController
     end
   end
 
+  #Экшн возвращаетвсе книги у автора 
+  def show_books
+    @book = Book.where(author_id: params[:id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_author
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :authors
   resources :books
+  resources :authors do
+    member do
+      get 'show_books'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
